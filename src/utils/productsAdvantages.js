edfc59bb93f7136dc424013a98fd6edca43d3578
KLaZ <muhabdanulikhlas0983@gmail.com>
2024-09-11 23:03:18 +0700

    membuat section 1 dan 2 untuk tampilann mobile

--- src/utils/productsAdvantages.js
+++ src/utils/productsAdvantages.js
@@ -1,21 +1,21 @@
 export const productsAdvantages = [
   {
     id: 1,
-    image: "images/svg/advantage-icon-1.svg",
+    image: "svg/advantage-icon-1.svg",
     title: "Ramah Lingkungan",
     description:
       "Terbuat dari bahan polyester ramah lingkungan dan limbah tekstil batik",
   },
   {
     id: 2,
-    image: "images/svg/advantage-icon-2.svg",
+    image: "svg/advantage-icon-2.svg",
     title: "Desain Menarik dan Fungsional",
     description:
       "Desain yang menarik dan fungsional cocok untuk berbagai kalangan, termasuk anak sekolah dan mahasiswa",
   },
   {
     id: 3,
-    image: "images/svg/advantage-icon-3.svg",
+    image: "svg/advantage-icon-3.svg",
     title: "Dukungan Produk Lokal",
     description:
       "Mendukung ekonomi lokal dengan penggunaan bahan dari komunitas setempat",
