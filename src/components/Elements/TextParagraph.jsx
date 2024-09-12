1038b32c77f66bb44400699e2c0d4a41d0780b96
KLaZ <muhabdanulikhlas0983@gmail.com>
2024-09-12 22:20:30 +0700

    update komponen untuk tampilan tablet dan dekstop

--- src/components/Elements/TextParagraph.jsx
+++ src/components/Elements/TextParagraph.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const TextParagraph = ({ children, customClassname }) => {
   return (
-    <p className={`${customClassname} text-[12px] mt-1 font-Poppins`}>
+    <p className={`${customClassname} text-[12px] mt-1 font-Poppins size1024:text-[16px] lg:text-[20px]`}>
       {children}
     </p>
   );
