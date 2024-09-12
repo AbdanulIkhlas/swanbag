a09cb38a20bd75113714dfb70b2b4868fa7a0fb2
KLaZ <muhabdanulikhlas0983@gmail.com>
2024-09-12 10:24:25 +0700

    update cardProduk

--- src/components/Fragments/CardProducts.jsx
+++ src/components/Fragments/CardProducts.jsx
@@ -3,14 +3,14 @@ import rupiahFormater from "../../utils/RupiahFormater";
 const CardProducts = ({ data }) => {
   return (
     <>
-      <div>
-        <div className="drop-shadow-md">
-          <img src={data.image} alt="" className="object-fill" />
+      <div className="">
+        <div className="drop-shadow-md ">
+          <img src={data.image} alt="" className="object-fill rounded-md" />
         </div>
-        <h3 className="text-xs md:text-base font-semibold text-[#373737] mt-2.5 mt-1.5">
+        <h3 className="text-xs md:text-base font-semibold text-[#373737] mt-3">
           {data.name}
         </h3>
-        <p className="text-[10px] md:text-base text-[#454545] font-medium">
+        <p className="text-[10px] md:text-base text-[#454545] font-medium mt-[2px]">
           {rupiahFormater(data.price)}
         </p>
       </div>
